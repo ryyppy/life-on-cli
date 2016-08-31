badbafee206ae7e55429d613cf311f1243645fe3
Patrick Stapfer <patrick.stapfer@runtastic.com>
2016-08-31 16:35:30 +0200

    Refine example1

--- examples/src/example1.js
+++ examples/src/example1.js
@@ -1,30 +1,56 @@
 // TODO: Show flow / eslint support integration
+
 /* eslint-disable */
 
-import data from './data'
 import React from 'react';
 
-// TODO: Copy attr1 sometimes and increment number
-// TODO: Remove the double-ticks for attribute names afterwards
-const someRandomJSON = {
-  attr1: "value",
+// TODO: Fix data
+import data from './data';
+
+// TODO: Extract attr1 from `source` into this object
+// TODO: Switch from double quotes to single quotes
+// TODO: Copy attr1 10 times and increment the number
+// TODO: Get rid of single quotes of all attributes
+const target = {
+};
+
+const source = {
+  'attr1': "test",
+};
+
+// TODO: Rename `sourceFile` to `targetFile` without renaming
+//       other things out of this scope
+function someFunctionScope(sourceFile) {
+  if (sourceFile === '/home/ryyppy') {
+    return `Hi ryyppy! This target is your home: ${sourceFile}`;
+  }
+  return `Hi! Target file: ${sourceFile}`;
 }
 
+type Props = {
+  // TODO: Rename something to 'person'
+  something: {
+    firstName: string,
+    email: string,
+  },
+};
+
 class SomeComponent extends React.Component {
+  props: Props;
+
   render() {
-    // TODO: Rename something to 'person' 
     const { something } = this.props;
 
     const {
-      first_name = 'None',
+      firstName = 'None',
       email = 'Nomail',
     } = something || {};
 
     // TODO: Append email as template string
-    const str = first_name;
+    const str = firstName;
 
     // TODO: Fix attribute alignment
-    // TODO: Surround with return (); 
+    // TODO: Surround with return ();
     <div id="mydiv" className="foo" ref="mydiv-ref">
       { str }
     </div>
@@ -35,15 +61,17 @@ class MyApp extends React.Component {
   render() {
     const something = null;
 
-    return (<SomeComponent something={something}/>)
+    return (<SomeComponent something={something} />);
   }
 }
 
+type User = { email: string };
+
 // TODO: Coworker insisted that this function
 //       needs at least one test :-(
-export function filterByEmail(data: Array<Object>, email: string): ?Object {
+export function filterByEmail(data: Array<User>, email: string): ?Object {
   if (data == null) {
-    return null; 
+    return null;
   }
 
   return data.find((ds) => ds.email === email);
